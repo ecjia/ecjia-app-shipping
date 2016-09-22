09fe77c6b057beb69981eb0c81f2f5391dea0d33
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 20:36:13 +0800

    app下model修改

--- model/shipping_area_model.class.php
+++ model/shipping_area_model.class.php
@@ -4,8 +4,6 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class shipping_area_model extends Component_Model_Model {
 	public $table_name = '';
 	public function __construct() {
-		$this->db_config = RC_Config::load_config('database');
-		$this->db_setting = 'default';
 		$this->table_name = 'shipping_area';
 		parent::__construct();
 	}
--- model/shipping_area_region_viewmodel.class.php
+++ model/shipping_area_region_viewmodel.class.php
@@ -6,8 +6,6 @@ class shipping_area_region_viewmodel extends Component_Model_View {
 	public $view = array();
 	
 	public function __construct() {
-		$this->db_config = RC_Config::load_config('database');
-		$this->db_setting = 'default';
 		$this->table_name = 'area_region';
 		$this->table_alias_name	= 'a';
 		
