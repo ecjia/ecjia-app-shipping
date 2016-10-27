fd90d2bf7cf4b288e8ea1d8bf86c7237c77e66ca
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-27 10:48:23 +0800

    修改配送方式

--- merchant.php
+++ merchant.php
@@ -59,116 +59,80 @@ class merchant extends ecjia_merchant {
 			'<p>' . __('<a href="https://ecjia.com/wiki/帮助:ECJia智能后台:配送方式" target="_blank">'. RC_Lang::get('shipping::shipping.about_shipping_list') .'</a>') . '</p>'
 		);
 		$this->assign('ur_here', '我的配送');
-
-		//替换数据库获取数据方式
-// 		$data = $this->db_shipping->shipping_select(array('shipping_id', 'shipping_code', 'shipping_name', 'shipping_desc', 'insure', 'support_cod', 'shipping_order', 'enabled'), '', 'shipping_order');
-		$type = isset($_GET['type']) ? trim($_GET['type']) : '';
-		$shipping_db = RC_Model::model('shipping/shipping_viewmodel');
-		$shipping_db->view = array(
-				'shipping_area' => array(
-						'type' 	=> Component_Model_View::TYPE_LEFT_JOIN,
-						'alias' => 'a',
-						'on' 	=> 'a.shipping_id = s.shipping_id and a.store_id ='.$_SESSION['store_id'],
-				),
-		);
-		$where = array('enabled' => 1);
-// 		, 'store_id' => $_SESSION['store_id']
-		$shppint_count	= $shipping_db->join(array('shipping_area'))->where($where)->group(array('s.shipping_id'))->select();
-		$enable_count	= $shipping_db->join(array('shipping_area'))->where(array_merge($where, array('a.shipping_area_id is not null')))->group(array('s.shipping_id'))->select();
-		$unable_count	= $shipping_db->join(array('shipping_area'))->where(array_merge($where, array('a.shipping_area_id is null')))->group(array('s.shipping_id'))->select();
-		$shppint_count['count']		   = count($shppint_count);
-		$shppint_count['enable_count'] = count($enable_count);
-		$shppint_count['unable_count'] = count($unable_count);
-// 		$count = $shppint_count['count'];
-// 		if ($type == 'enable') {
-// 			$count = $shppint_count['enable_count'];
-// 			$where[] = 'a.shipping_area_id is not null';
-// 		} elseif ($type == 'unable') {
-// 			$count = $shppint_count['unable_count'];
-// 			$where[] = 'a.shipping_area_id is null';
-// 		}
-
-		$count_enable = $shppint_count['enable_count'];
-		$enable_where[] = 'a.shipping_area_id is not null';
-		$enable_page = new ecjia_merchant_page($count_enable, 3, 3);
-
-		$count_unable = $shppint_count['unable_count'];
-		$unable_where[] = 'a.shipping_area_id is null';
-		$unable_page = new ecjia_merchant_page($count_unable, 3, 3);
-
-// 		$data = $shipping_db->field(array('s.*', 'a.shipping_area_id'))->join(array('shipping_area'))->where($where)->order(array('shipping_order' => 'asc'))->group(array('s.shipping_id'))->limit($page->limit())->select();
-		$enable_data = $shipping_db->field(array('s.*', 'a.shipping_area_id'))->join(array('shipping_area'))->where($enable_where)->order(array('shipping_order' => 'asc'))->group(array('s.shipping_id'))->limit($enable_page->limit())->select();
-		$unable_data = $shipping_db->field(array('s.*', 'a.shipping_area_id'))->join(array('shipping_area'))->where($unable_where)->order(array('shipping_order' => 'asc'))->group(array('s.shipping_id'))->limit($unable_page->limit())->select();
+		
+		//已启用的配送方式
+		$enabled_data = RC_DB::table('shipping')
+			->select('*')
+			->where('enabled', 1)
+			->orderBy('shipping_order')
+			->get();
+		
+		//未启用的配送方式
+		$disabled_data = RC_DB::table('shipping')
+			->select('*')
+			->where('enabled', 0)
+			->orderBy('shipping_order')
+			->get();
 		
 		$plugins = ecjia_config::instance()->get_addon_config('shipping_plugins', true);
 		
 		/* 插件已经安装了，获得名称以及描述 */
-		$modules = $enable_shipping = $unable_shipping = array();
-		if (!empty($enable_data)) {
-			foreach ($enable_data as $_key => $_value) {
-				if (isset($plugins[$_value['shipping_code']])) {
-					$enable_shipping[$_key]['id']      			= $_value['shipping_id'];
-					$enable_shipping[$_key]['code']      		= $_value['shipping_code'];
-					$enable_shipping[$_key]['name']    			= $_value['shipping_name'];
-					$enable_shipping[$_key]['desc']    			= $_value['shipping_desc'];
-					$enable_shipping[$_key]['cod']     			= $_value['support_cod'];
-					$enable_shipping[$_key]['shipping_order'] 	= $_value['shipping_order'];
-					$enable_shipping[$_key]['insure_fee']  		= $_value['insure'];
-					$enable_shipping[$_key]['enabled'] 			= $_value['enabled'];
-					 
-					/* 判断该派送方式是否支持保价 支持报价的允许在页面修改保价费 */
-					$shipping_handle = new shipping_factory($_value['shipping_code']);
-					$config          = $shipping_handle->configure_config();
-			
-					/* 只能根据配置判断是否支持保价  只有配置项明确说明不支持保价，才是不支持*/
-					if (isset($config['insure']) && ($config['insure'] === false)) {
-						$enable_shipping[$_key]['is_insure'] = false;
-					} else {
-						$enable_shipping[$_key]['is_insure'] = true;
-					}
+		$enabled_modules = $disabled_modules = array();
+		
+		//已启用
+		foreach ($enabled_data as $_key => $_value) {
+			if (isset($plugins[$_value['shipping_code']])) {
+				$enabled_modules[$_key]['id']      			= $_value['shipping_id'];
+				$enabled_modules[$_key]['code']      		= $_value['shipping_code'];
+				$enabled_modules[$_key]['name']    			= $_value['shipping_name'];
+				$enabled_modules[$_key]['desc']    			= $_value['shipping_desc'];
+				$enabled_modules[$_key]['cod']     			= $_value['support_cod'];
+				$enabled_modules[$_key]['shipping_order'] 	= $_value['shipping_order'];
+				$enabled_modules[$_key]['insure_fee']  		= $_value['insure'];
+				$enabled_modules[$_key]['enabled'] 			= $_value['enabled'];
+				 
+				/* 判断该派送方式是否支持保价 支持报价的允许在页面修改保价费 */
+				$shipping_handle = new shipping_factory($_value['shipping_code']);
+				$config          = $shipping_handle->configure_config();
+		
+				/* 只能根据配置判断是否支持保价  只有配置项明确说明不支持保价，才是不支持*/
+				if (isset($config['insure']) && ($config['insure'] === false)) {
+					$enabled_modules[$_key]['is_insure'] = false;
+				} else {
+					$enabled_modules[$_key]['is_insure'] = true;
 				}
 			}
 		}
+		$this->assign('enabled', $enabled_modules);
 		
-		if (!empty($unable_data)) {
-		    foreach ($unable_data as $_key => $_value) {
-		        if (isset($plugins[$_value['shipping_code']])) {
-		            $unable_shipping[$_key]['id']      			= $_value['shipping_id'];
-		            $unable_shipping[$_key]['code']      		= $_value['shipping_code'];
-		            $unable_shipping[$_key]['name']    			= $_value['shipping_name'];
-		            $unable_shipping[$_key]['desc']    			= $_value['shipping_desc'];
-		            $unable_shipping[$_key]['cod']     			= $_value['support_cod'];
-		            $unable_shipping[$_key]['shipping_order'] 	= $_value['shipping_order'];
-		            $unable_shipping[$_key]['insure_fee']  		= $_value['insure'];
-		            $unable_shipping[$_key]['enabled'] 			= $_value['enabled'];
+		//未启用
+		foreach ($disabled_data as $_key => $_value) {
+			if (isset($plugins[$_value['shipping_code']])) {
+				$disabled_modules[$_key]['id']      			= $_value['shipping_id'];
+				$disabled_modules[$_key]['code']      		= $_value['shipping_code'];
+				$disabled_modules[$_key]['name']    			= $_value['shipping_name'];
+				$disabled_modules[$_key]['desc']    			= $_value['shipping_desc'];
+				$disabled_modules[$_key]['cod']     			= $_value['support_cod'];
+				$disabled_modules[$_key]['shipping_order'] 	= $_value['shipping_order'];
+				$disabled_modules[$_key]['insure_fee']  		= $_value['insure'];
+				$disabled_modules[$_key]['enabled'] 			= $_value['enabled'];
+					
+				/* 判断该派送方式是否支持保价 支持报价的允许在页面修改保价费 */
+				$shipping_handle = new shipping_factory($_value['shipping_code']);
+				$config          = $shipping_handle->configure_config();
 		
-		            /* 判断该派送方式是否支持保价 支持报价的允许在页面修改保价费 */
-		            $shipping_handle = new shipping_factory($_value['shipping_code']);
-		            $config          = $shipping_handle->configure_config();
-		            	
-		            /* 只能根据配置判断是否支持保价  只有配置项明确说明不支持保价，才是不支持*/
-		            if (isset($config['insure']) && ($config['insure'] === false)) {
-		                $unable_shipping[$_key]['is_insure'] = false;
-		            } else {
-		                $unable_shipping[$_key]['is_insure'] = true;
-		            }
-		        }
-		    }
+				/* 只能根据配置判断是否支持保价  只有配置项明确说明不支持保价，才是不支持*/
+				if (isset($config['insure']) && ($config['insure'] === false)) {
+					$disabled_modules[$_key]['is_insure'] = false;
+				} else {
+					$disabled_modules[$_key]['is_insure'] = true;
+				}
+			}
 		}
-   
-		$this->assign('enable_shipping', $enable_shipping);
-		$this->assign('unable_shipping', $unable_shipping);
-// 		$this->assign('shipping_list', $modules);
-// 		$this->assign('page', $page->show(5));
-		$this->assign('enable_page', $enable_page->show(5));
-		$this->assign('unable_page', $unable_page->show(5));
-		$this->assign('shipping_count', $shppint_count);
+		$this->assign('disabled', $disabled_modules);
 		
 		$this->display('shipping_list.dwt');
 	}
-	
-	
-	
 }	
 
 // end
--- templates/merchant/shipping_list.dwt.php
+++ templates/merchant/shipping_list.dwt.php
@@ -18,136 +18,120 @@
 
 <div class="row">
 	<div class="col-lg-12">
-<!-- 	    <div class="panel"> -->
-<!-- 	    	<div class="panel-body"> -->
-				<div class="row-fluid">
-					<div class="accordion-group panel panel-default">
-					<div class="panel-heading panel-body ">
+	    <div class="panel">
+	    	<div class="panel-body">
+				<div class="accordion-group panel panel-default">
+					<div class="panel-heading">
                         <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
                             <h4 class="panel-title">
                                 <strong>{t}已启用{/t}</strong>
                             </h4>
                         </a>
                      </div>
-					<section class="panel panel-body">
-						<table class="table table-striped table-hide-edit">
-							<thead>
-								<tr>
-									<th class="w110">{lang key='shipping::shipping.shipping_name'}</th>
-									<th>{lang key='shipping::shipping.shipping_desc'}</th>
-									<th class="w80">{lang key='system::system.sort_order'}</th>
-									<th class="w80">{lang key='shipping::shipping.insure'}</th>
-									<th class="w100">{lang key='shipping::shipping.support_cod'}</th>
-								</tr>
-							</thead>
-							<tbody>
-								<!-- {foreach from=$enable_shipping item=module} -->
-								<tr>
-									<td>
-										{$module.name}
-									</td>
-									<td class="hide-edit-area">
-									<!-- {if $module.enabled == 1} -->
-											{$module.desc|nl2br}
-											<div class="edit-list">
-												<a class="data-pjax" href='{RC_Uri::url("shipping/mh_area/init", "shipping_id={$module.id}&code={$module.code}")}'  title="{lang key='shipping::shipping.shipping_area'}">{lang key='shipping::shipping.set_shipping'}</a>
-											</div>
-										<!-- {else} -->
-											{$module.desc|nl2br}
-										<!-- {/if} -->
-										
-									</td>
-									<td>
-										{$module.shipping_order}
-									</td>
-									<td>
-										<!-- {if $module.is_insure } -->
-											{$module.insure_fee}
-										<!-- {else} -->
-											{lang key='shipping::shipping.not_support'}
-										<!-- {/if} -->
-									</td>
-									<td>
-										{if $module.cod==1}
-											{lang key='system::system.yes'}
-										{else}
-											{lang key='system::system.no'}
-										{/if}
-									</td>
-								</tr>
-								<!-- {foreachelse} -->
-								<tr><td class="no-records" colspan="10">{lang key='system::system.no_records'}</td></tr>
-								<!-- {/foreach} -->
-							</tbody>
-						</table>
-					</section>
-					<!-- {$enable_page} -->
+					<table class="table table-striped table-hide-edit">
+						<thead>
+							<tr>
+								<th class="w110">{lang key='shipping::shipping.shipping_name'}</th>
+								<th>{lang key='shipping::shipping.shipping_desc'}</th>
+								<th class="w80">{lang key='system::system.sort_order'}</th>
+								<th class="w80">{lang key='shipping::shipping.insure'}</th>
+								<th class="w100">{lang key='shipping::shipping.support_cod'}</th>
+							</tr>
+						</thead>
+						<tbody>
+							<!-- {foreach from=$enabled item=module} -->
+							<tr>
+								<td>
+									{$module.name}
+								</td>
+								<td class="hide-edit-area">
+									{$module.desc|nl2br}
+									<div class="edit-list">
+										<a class="data-pjax" href='{RC_Uri::url("shipping/mh_area/init", "shipping_id={$module.id}&code={$module.code}")}'  title="{lang key='shipping::shipping.shipping_area'}">{lang key='shipping::shipping.set_shipping'}</a>
+									</div>
+								</td>
+								<td>
+									{$module.shipping_order}
+								</td>
+								<td>
+									<!-- {if $module.is_insure } -->
+										{$module.insure_fee}
+									<!-- {else} -->
+										{lang key='shipping::shipping.not_support'}
+									<!-- {/if} -->
+								</td>
+								<td>
+									{if $module.cod==1}
+										{lang key='system::system.yes'}
+									{else}
+										{lang key='system::system.no'}
+									{/if}
+								</td>
+							</tr>
+							<!-- {foreachelse} -->
+							<tr><td class="no-records" colspan="5">{lang key='system::system.no_records'}</td></tr>
+							<!-- {/foreach} -->
+						</tbody>
+					</table>
 				</div>
-				</div>
-				
-				<div class="row-fluid">
+			</div>
+		</div>
+		
+		<div class="panel">	
+			<div class="panel-body">			
 				<div class="accordion-group panel panel-default">
 					<div class="panel-heading">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                            <h4 class="panel-title">
-                                <strong>{t}未启用{/t}</strong>
-                            </h4>
-                        </a>
-                     </div>
-					<section class="panel">
-						<table class="table table-striped table-hide-edit">
-							<thead>
-								<tr>
-									<th class="w110">{lang key='shipping::shipping.shipping_name'}</th>
-									<th>{lang key='shipping::shipping.shipping_desc'}</th>
-									<th class="w80">{lang key='system::system.sort_order'}</th>
-									<th class="w80">{lang key='shipping::shipping.insure'}</th>
-									<th class="w100">{lang key='shipping::shipping.support_cod'}</th>
-								</tr>
-							</thead>
-							<tbody>
-								<!-- {foreach from=$unable_shipping item=module} -->
-								<tr>
-									<td>
-										{$module.name}
-									</td>
-									<td class="hide-edit-area">
-									<!-- {if $module.enabled == 1} -->
-											{$module.desc|nl2br}
-											<div class="edit-list">
-												<a class="data-pjax" href='{RC_Uri::url("shipping/mh_area/init", "shipping_id={$module.id}&code={$module.code}")}'  title="{lang key='shipping::shipping.shipping_area'}">{lang key='shipping::shipping.set_shipping'}</a>
-											</div>
-										<!-- {else} -->
-											{$module.desc|nl2br}
-										<!-- {/if} -->
-										
-									</td>
-									<td>
-										{$module.shipping_order}
-									</td>
-									<td>
-										<!-- {if $module.is_insure } -->
-											{$module.insure_fee}
-										<!-- {else} -->
-											{lang key='shipping::shipping.not_support'}
-										<!-- {/if} -->
-									</td>
-									<td>
-										{if $module.cod==1}
-											{lang key='system::system.yes'}
-										{else}
-											{lang key='system::system.no'}
-										{/if}
-									</td>
-								</tr>
-								<!-- {foreachelse} -->
-								<tr><td class="no-records" colspan="10">{lang key='system::system.no_records'}</td></tr>
-								<!-- {/foreach} -->
-							</tbody>
-						</table>
-					</section>
-					 <!-- {$unable_page} -->
-				    </div>
+	          			<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+	                 		<h4 class="panel-title">
+	          					<strong>{t}未启用{/t}</strong>
+	                        </h4>
+	              		</a>
+	     			</div>
+					<table class="table table-striped table-hide-edit">
+						<thead>
+							<tr>
+								<th class="w110">{lang key='shipping::shipping.shipping_name'}</th>
+								<th>{lang key='shipping::shipping.shipping_desc'}</th>
+								<th class="w80">{lang key='system::system.sort_order'}</th>
+								<th class="w80">{lang key='shipping::shipping.insure'}</th>
+								<th class="w100">{lang key='shipping::shipping.support_cod'}</th>
+							</tr>
+						</thead>
+						<tbody>
+							<!-- {foreach from=$disabled item=module} -->
+							<tr>
+								<td>
+									{$module.name}
+								</td>
+								<td class="hide-edit-area">
+									{$module.desc|nl2br}
+									<div class="edit-list">
+										<a class="data-pjax" href='{RC_Uri::url("shipping/mh_area/init", "shipping_id={$module.id}&code={$module.code}")}'  title="{lang key='shipping::shipping.shipping_area'}">{lang key='shipping::shipping.set_shipping'}</a>
+									</div>
+								</td>
+								<td>
+									{$module.shipping_order}
+								</td>
+								<td>
+									<!-- {if $module.is_insure } -->
+										{$module.insure_fee}
+									<!-- {else} -->
+										{lang key='shipping::shipping.not_support'}
+									<!-- {/if} -->
+								</td>
+								<td>
+									{if $module.cod==1}
+										{lang key='system::system.yes'}
+									{else}
+										{lang key='system::system.no'}
+									{/if}
+								</td>
+							</tr>
+							<!-- {foreachelse} -->
+							<tr><td class="no-records" colspan="5">{lang key='system::system.no_records'}</td></tr>
+							<!-- {/foreach} -->
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
