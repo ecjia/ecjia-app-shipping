0319e455703ebb854ced32fca0830a99232fabdb
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-30 11:24:28 +0800

    更新样式

--- templates/admin/shipping_template.dwt.php
+++ templates/admin/shipping_template.dwt.php
@@ -518,12 +518,12 @@
 			    	<fieldset>
 				        <div class="chat_box library-content">
 				        	<div class="row-fluid">
-				                <div class="span9 chat_content template_info">
+				                <div class="span9" id="code_shipping_print" {if $shipping.print_model == 2}style="display:none"{/if}>
 			                        <div class="chat_heading clearfix">
 			                            <span class="title">代码模式</span>
 			                        </div>
-				  					<div class="table_box" id="code_shipping_print" {if $shipping.print_model == 2}style="display:none"{/if} class="span9">
-					       				<textarea style="width:819px;height:556px;" id="shipping_print" name="shipping_print" rows="40" cols="3" >{$shipping.shipping_print|escape:html}</textarea>
+				  					<div class="table_box" class="span9">
+					       				<textarea style="width:820px;height:556px;" id="shipping_print" name="shipping_print" rows="40" cols="3" >{$shipping.shipping_print|escape:html}</textarea>
 					                </div>
 				                </div>
 				                
