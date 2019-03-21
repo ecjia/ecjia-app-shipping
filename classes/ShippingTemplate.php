e98437d6f69880f16da63003e5dc6e2e7adff5c5
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-21 17:17:06 +0800

    封装快递单打印数据

--- classes/ShippingTemplate.php
+++ classes/ShippingTemplate.php
@@ -9,12 +9,8 @@
 namespace Ecjia\App\Shipping;
 
 
-class ShippingTemplate
+class ShippingTemplate extends ShippingTemplateAbstract
 {
-
-    protected $default = [];
-
-    protected $setting = [];
 
     public function __construct()
     {
@@ -47,40 +43,6 @@ class ShippingTemplate
 	        'order_best_time'       => __('表示最佳送货时间', 'shipping'),
         ];
     }
-
-    /**
-     * 设置变量数据
-     * @param $key
-     * @param $value
-     * @return $this
-     */
-    public function setTemplateData($key, $value)
-    {
-        $keys = array_keys($this->default);
-        if (in_array($key, $keys)) {
-            $this->setting[$key] = $value;
-        }
-
-        return $this;
-    }
-
-    /**
-     * 获取已经成功设置过的数据
-     * @return array
-     */
-    public function getTemplateData()
-    {
-        return $this->setting;
-    }
-
-    /**
-     * 获取默认选项
-     */
-    public function getDefaults()
-    {
-        return $this->default;
-    }
-
     
     /**
      * 获取默认选项，并格式化输出
@@ -88,7 +50,7 @@ class ShippingTemplate
     public function getDefaultsWithFormatted()
     {
     	return collect($this->default)->map(function($item, $key) {
-    		return '<font class="ecjiafc-blue">{$'. $key .'}</font> ' . $item;
+    		return '<span class="ecjiafc-blue">{$'. $key .'}</span> ' . $item;
     	})->all();
     }
 }
